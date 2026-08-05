0eb4dbf1cf9e2da56de6f65d263c33fe2e16236b
Muhibul <muhibulalam911@gmail.com>
2026-08-05 16:20:50 +0600

    Implement route optimization for minimum emissions
    Modified the code to find the route with minimum carbon emissions based on distances between ports.

--- 1.2-Building-AI/chapter_1/exercise_2.py
+++ 1.2-Building-AI/chapter_1/exercise_2.py
@@ -44,3 +44,74 @@ def main():
 
     permutation([0],list(range(1, len(portnames))))
 main()
+
+"""
+Building on the previous solution, modify the code so that it finds the route with minimum carbon emissions and prints it out. Again, the program should work for any number of ports. You can assume that the distances between the ports are given in an array of the appropriate size so that the distance between ports i and j is found in D[i][j].
+
+Output Example
+PAN AMS CAS NYC HEL 240.1 kg
+Tip: Your values might be different, but the formatting should be identical.
+
+"""
+# Advance
+
+portnames = ["PAN", "AMS", "CAS", "NYC", "HEL"]
+
+# https://sea-distances.org/
+# nautical miles converted to km
+
+D = [
+        [0,8943,8019,3652,10545],
+        [8943,0,2619,6317,2078],
+        [8019,2619,0,5836,4939],
+        [3652,6317,5836,0,7825],
+        [10545,2078,4939,7825,0]
+    ]
+
+# https://timeforchange.org/co2-emissions-shipping-goods
+# assume 20g per km per metric ton (of pineapples)
+
+co2 = 0.020
+
+# DATA BLOCK ENDS
+
+# these variables are initialised to nonsensical values
+# your program should determine the correct values for them
+smallest = 1000000
+bestroute = [0, 0, 0, 0, 0]
+
+def permutations(route, ports):
+    # write the recursive function here
+    # remember to calculate the emissions of the route as the recursion ends
+    # and keep track of the route with the lowest emissions
+    global smallest, bestroute
+
+    if ((len(ports)) == 0):
+        distance = sum(D[route[i]][route[i+1]] for i in range (len(route) - 1))
+        emissions = distance * co2
+        
+        if (emissions < smallest):
+            smallest = emissions
+            bestroute = route
+        return
+
+
+    for i in (range(len(ports))):
+        new_routes = route + [ports[i]]
+        remaining_ports = ports[:i] + ports[i+1:]
+        permutations(new_routes, remaining_ports)
+
+def main():
+    # Do not edit any (global) variables using this function, as it will mess up the testing
+
+    # this will start the recursion 
+    
+    permutations([0], list(range(1, len(portnames))))
+
+    # print the best route and its emissions
+    print(' '.join([portnames[i] for i in bestroute]) + " %.1f kg" % smallest)
+
+main()
+
+
+
