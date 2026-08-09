7ed31b29db53071b5c4b3789e366f69c30eee001
Muhibul <muhibulalam911@gmail.com>
2026-08-10 01:22:47 +0600

    Modify climb function for extended movement range
    Updated the climbing algorithm to allow movement up to five steps left or right.
    exercise 3 (advance)

--- 1.2-Building-AI/chapter_1/exercise_3.py
+++ 1.2-Building-AI/chapter_1/exercise_3.py
@@ -49,5 +49,78 @@ def main(h):
 
 main(h)
 
+"""
+Exercise 3: Reach the highest summit
+Let the elevation at each point on the mountain be stored in array h of size 100. 
+The elevation at the leftmost point is thus stored in h[0] and the elevation at the rightmost point is stored in h[99].
+
+Here's an example chart. The green box represents Venla's starting position, and the red triangle marks the point where Venla stops. 
+A blue triangle (not shown here) marks the summit.
+
+The following program starts at a random position and keeps going to the right until Venla can no longer go up. 
+However, perhaps the mountain is a bit rugged which means it's necessary to look a bit further ahead.
+
+Edit the program so that Venla doesn't stop climbing as long as she can go up by moving up to five steps either left or right. 
+If there are multiple choices within five steps that go up, any one of them is good. 
+To check how your climbing algorithm works in action, you can plot the results of your hill climbing using the Plot button. 
+As a reminder, the summit will be marked with a blue triangle.
+
+"""
+
+# Advance
+
+# our solution
+import math
+import random             	# just for generating random mountains                                 	 
+
+# generate random mountains                                                                               	 
+
+w = [.05, random.random()/3, random.random()/3]
+h = [1.+math.sin(1+x/.6)*w[0]+math.sin(-.3+x/9.)*w[1]+math.sin(-.2+x/30.)*w[2] for x in range(100)]
+
+def climb(x, h):
+    # keep climbing until we've found a summit
+    summit = False
+    # edit here
+    while not summit:
+        
+        summit = True         # stop unless there's a way up
+        for i in range (1,6):
+
+            if x + i < 100 and h[x + i] > h[x]:
+                x = x + i        # right is higher, go there
+                summit = False
+                break    
+            elif x - i >= 0 and h[x - i] > h[x]:
+                x = x - i
+                summit = False
+                break
+    return x
+
+def main(h):
+    # start at a random place                                                                                  	 
+    x0 = random.randint(1, 98)
+    x = climb(x0, h)
+
+    return x0, x
+
+main(h)
 
 
+
+# Program's Solutions
+
+# Example solution
+
+def climb(x):
+    # keep climbing until we've found a summit
+    summit = False
+
+    # edit here
+    while not summit:
+        summit = True
+        for x_new in range(max(0, x-5), min(99, x+5)):
+            if h[x_new] > h[x]:
+                x = x_new         # here is higher, go here 
+                summit = False    # and keep going
+    return x
