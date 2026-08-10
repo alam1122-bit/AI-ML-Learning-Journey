25ecde9386fb6d501f9b7848972f2bb1bf580e7b
Muhibul <muhibulalam911@gmail.com>
2026-08-10 15:24:20 +0600

    Modify probabilities for dog and cat output
    Updated the program to print 'dog' with 80% probability and 'cat' with 20%.
    
    exercise 4
    Intermediate level
    Building AI
    University of Helsinki

---
+++ 1.2-Building-AI/chapter_1/exercise_4.py
@@ -0,0 +1,22 @@
+"""
+Exercise 4: Probabilities
+Recall the program that prints out the word 'dog' with 20% probability. 
+Modify the program so that it prints either the word 'dog' or the word 'cat' (but never both, because either you're a dog person or a cat person, but not both, right?)
+
+Change the probability of the word 'dog' to be 80% probability (because apparently there are more dog lovers than cat lovers in the world) so that the probability of the word 
+'cat' is 20%.
+"""
+# Intermediate Level
+
+import random
+
+def main():
+    prob = 0.20
+    if random.random() < prob:
+        print('cat')
+    else:
+        print('dog')
+
+main()
+
+
