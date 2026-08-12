1bad8962a77ce695a88e2a961f45a51fa90e5dc2
Muhibul <muhibulalam911@gmail.com>
2026-08-12 23:27:40 +0600

    Add exercise 6 with questions about temperature in AI
    Building AI
    University of Helsinki

---
+++ 1.2-Building-AI/chapter_1/exercise_6.py
@@ -0,0 +1,13 @@
+"""
+Exercise 6 (Beginner Level)
+To understand what role temperature plays, it's worthwhile to pause here for a second to think about and answer the following questions:
+
+Question-01: What happens when we increase the temperature value to a ridiculously large number?
+
+ans: we accept all new solutions
+
+Question-02: What happens if we set the temperature as close as possible to 0?
+
+ans: we only accept new solutions when they are better than the current one
+
+"""
