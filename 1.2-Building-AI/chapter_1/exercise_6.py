52b5344003a536d911dea6d586664ef9ac19b09d
Muhibul <muhibulalam911@gmail.com>
2026-08-12 23:30:27 +0600

    Implement simulated annealing in exercise 6
    Modified the program to implement simulated annealing instead of plain hill climbing, including temperature adjustment logic.

--- 1.2-Building-AI/chapter_1/exercise_6.py
+++ 1.2-Building-AI/chapter_1/exercise_6.py
@@ -11,3 +11,23 @@ Question-02: What happens if we set the temperature as close as possible to 0?
 ans: we only accept new solutions when they are better than the current one
 
 """
+
+"""
+Exercise 6: Simulated Annealing (Intermediate Level)
+1D simulated annealing: modify the program below to use simulated annealing instead of plain hill climbing. 
+In simulated annealing the probability of accepting a solution that lowers the score is given by prob = exp(-(S_old - S_new)/T). 
+Setting the temperature T and gradually decreasing can be done in many ways, some of which lead to better outcomes than others. 
+A good choice in this case is for example: T = 2*max(0, ((steps-step*1.2)/steps))**3.
+
+Running the code produces something like the following chart, where the black box marks the starting point. 
+The code below uses the plain hill-climbing strategy to only go up towards a peak. The solution is marked by a red star. 
+As you can see, the hill-climbing strategy tends to get stuck in local optima.
+
+"""
+# Solution
+
+
+
+
+
+
