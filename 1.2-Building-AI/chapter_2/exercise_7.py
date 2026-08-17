88d09d87fdd37b6a472284f25555061de6771e98
Muhibul <muhibulalam911@gmail.com>
2026-08-17 22:31:46 +0600

    Add function to generate sequence and count '11111'
    Implement a function to generate a sequence of random zeros and ones based on a given probability and count occurrences of five consecutive ones.
    
    Exercise 7
    advance level
    probability

--- 1.2-Building-AI/chapter_2/exercise_7.py
+++ 1.2-Building-AI/chapter_2/exercise_7.py
@@ -33,8 +33,44 @@ def count11(seq):
 print(count11([0, 0, 1, 1, 1, 0])) # this should print 2
 
 
+"""
+Advance level
 
+Write a program that generates 10000 random zeros and ones where the probability of one is p1 and the probability 
+of zero is 1-p1 (hint: np.random.choice([0,1], p=[1-p1, p1], size=10000)), counts the number of occurrences of 5 consecutive ones ("11111") in the sequence, 
+and outputs this number as a return value. 
+Check that for p1 = 2/3, the count is close to 10000 x (2/3)^5 ≈ 1316.9.
+"""
 
+import numpy as np
+
+def generate(p1):
+    seq = np.random.choice([0 ,1],p = [1 - p1, p1],size=10000)
+    # change this so that it generates 10000 random zeros and ones
+    # where the probability of one is p1
+
+    # seq = np.empty(10000)
+    return seq
+
+def count(seq):
+    x=0
+    for i in range(len(seq)-4):
+        if seq[i] == 1 and seq[i+1] == 1 and seq[i+2] == 1 and seq[i+3] == 1 and seq[i+4] == 1:
+            x = x + 1
+    return x
+
+def main(p1):
+    seq = generate(p1)
+    return count(seq)
+
+print(main(2/3))
+
+"""
+The probability of "11111" at any given position in the sequence can be calculated as (2/3)^5 ≈ 0.13169. 
+The number of occurrences is close to 10000 times this: 1316.9. To be more precise, the expected number of occurrences is about 0.13169 x 9996 ≈ 1316.3, 
+because there are only 9996 places for a subsequence of length five in a sequence of 10000. The actual number will usually (in fact, with over 99% probability) 
+be somewhere between 1230 and 1404. We check the solution allowing for an even wider margin that covers 99.99% of the cases.
+"""
 
 
 
