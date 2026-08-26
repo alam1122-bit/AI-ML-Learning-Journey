c0fa52ede57458231f7ee2b053419f0beb3393bb
Muhibul <muhibulalam911@gmail.com>
2026-08-26 16:35:09 +0600

    Add naive Bayes logic to evaluate loaded die
    Implement naive Bayes method to determine if the loaded die is more likely based on the outcomes of dice rolls.

--- 1.2-Building-AI/chapter_2/exercise_10.py
+++ 1.2-Building-AI/chapter_2/exercise_10.py
@@ -61,6 +61,41 @@ Your task: starting from the odds 1:1, use the naive Bayes method to update the 
 Edit the function bayes so that it returns True if the most likely die is the loaded one, and False otherwise. 
 Remember to be careful with the indices when accessing list elements!
 """
+import numpy as np
+
+p1 = [1/6, 1/6, 1/6, 1/6, 1/6, 1/6]   # normal
+p2 = [0.1, 0.1, 0.1, 0.1, 0.1, 0.5]   # loaded
+
+def roll(loaded):
+    if loaded:
+        print("rolling a loaded die")
+        p = p2
+    else:
+        print("rolling a normal die")
+        p = p1
+
+    # roll the dice 10 times
+    # add 1 to get dice rolls from 1 to 6 instead of 0 to 5
+    sequence = np.random.choice(6, size=10, p=p) + 1 # e.g. [1,5,2,...4]
+    for roll in sequence:
+        print("rolled %d" % roll)
+        
+    return sequence
+
+def bayes(sequence):
+    odds = 1.0           # start with odds 1:1
+    for roll in sequence:
+         odds *=  p2[roll - 1] / p1[roll - 1]             # edit here to update the odds
+    if odds > 1:
+        return True
+    else:
+        return False
+
+sequence = roll(True)
+if bayes(sequence):
+    print("I think loaded")
+else:
+    print("I think normal")
 
 
 
