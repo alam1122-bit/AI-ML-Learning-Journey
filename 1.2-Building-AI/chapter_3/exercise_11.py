986cba2c599c664984626fa800665e8b971757bb
Muhibul <muhibulalam911@gmail.com>
2026-08-27 23:20:27 +0600

    Modify predict function for multiple cabin processing
    Updated the predict function to handle multiple cabins with varying details. Adjusted input values and coefficients accordingly.

--- 1.2-Building-AI/chapter_3/exercise_11.py
+++ 1.2-Building-AI/chapter_3/exercise_11.py
@@ -51,5 +51,24 @@ def predict(X, c):
 
 predict(X, c)
 
+"""
+Edit the following program so that it can process multiple cabins that may be described by any number of details (like five below), at the same time. 
+You can assume that each of the lists contained in the list x and the coefficients c contain the same number of elements.
+"""
+# input values for three mökkis: size, size of sauna, distance to water, number of indoor bathrooms, 
+# proximity of neighbors
+X = [[66, 5, 15, 2, 500], 
+     [21, 3, 50, 1, 100], 
+     [120, 15, 5, 2, 1200]]
+c = [3000, 200, -50, 5000, 100]    # coefficient values
 
+def predict(X, c):
+    
+    for i in range(len(X)):
+        price = 0
+        for j in range(len(c)):
+            price += c[j] * X[i][j]            
+        print(price)
+
+predict(X, c)
 
