1617a3e4011b45edede44155f0256eb0c1999ffe
Muhibul <muhibulalam911@gmail.com>
2026-08-27 20:26:14 +0600

    Update predict function to handle multiple cabins
    Modified the code to print predicted prices for multiple cabins based on their attributes.

--- 1.2-Building-AI/chapter_3/exercise_11.py
+++ 1.2-Building-AI/chapter_3/exercise_11.py
@@ -22,3 +22,34 @@ number of indoor toilets: 1, distance to next door neighbor: 200m.
 
 Your answer is correct: 492450 euro
 """
+# Level - Intermediate
+# Edit the code so that it prints out the prices of multiple cabins in one go.
+
+# input values for three mökkis: 
+#  - size [m^2], 
+#  - size of the sauna [m^2], 
+#  - distance to water [m], 
+#  - number of indoor bathrooms, 
+#  - proximity of neighbors [m]
+X = [[66, 5, 15, 2, 500], 
+     [21, 3, 50, 1, 100], 
+     [120, 15, 5, 2, 1200]]
+
+# coefficient values
+c = [3000, 200 , -50, 5000, 100]
+
+def predict(X, c):
+    # write a loop that goes over the cabin data and for each cabin prints out 
+    # the predicted price of the cabin you can assume that the number of inputs
+    # and the number of coefficients are the same
+    
+    for i in range(len(X)):
+        price = 0
+        for j in range(len(c)):
+            price += c[j] * X[i][j]
+        print(price)
+
+predict(X, c)
+
+
+
