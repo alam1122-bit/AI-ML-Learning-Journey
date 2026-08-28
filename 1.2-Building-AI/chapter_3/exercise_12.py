6f6c6da68e77c0d3a8e60202beb25ad612afc451
Muhibul <muhibulalam911@gmail.com>
2026-08-28 23:25:03 +0600

    Create exercise_12.py for model error calculation
    Added exercise 12 for beginner level on model fitting.

---
+++ 1.2-Building-AI/chapter_3/exercise_12.py
@@ -0,0 +1,17 @@
+"""
+Level - Beginner
+
+Suppose we have a data set of three points in the format of (x, y): (0, 5), (2, 9.6), and (3.2, 13.6). Your colleagues have built three different models to fit the data, 
+all of the form y = a + b*x. They have given you the coefficients of their models, and they are as follows:
+
+a) a=0.5, b=3.2
+
+b) a=0, b=1.9
+
+c) a=7.6, b=1.9
+
+Which of the coefficients gives the smallest squared error? You can use the "Beginner Exercise Dataset" in the above widget to help you calculate the error!
+
+Select the correct answer : c) a=7.6, b=1.9
+
+"""
