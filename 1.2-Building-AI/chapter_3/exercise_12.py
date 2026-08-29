421d66f474d279f2f83c13d98085f652d1f52e33
Muhibul <muhibulalam911@gmail.com>
2026-08-29 22:12:54 +0600

    Add squared error calculation in exercise_12.py
    Implement squared error calculation for predicted prices.

--- 1.2-Building-AI/chapter_3/exercise_12.py
+++ 1.2-Building-AI/chapter_3/exercise_12.py
@@ -17,3 +17,32 @@ Which of the coefficients gives the smallest squared error? You can use the "Beg
 Select the correct answer : c) a=7.6, b=1.9
 
 """
+"""
+Level - Intermediate
+Modify the program so it implements the calculation of the squared error. 
+In other words, you should calculate the predicted prices for all the cabins in the data, 
+subtract the predicted price from the actual price (which is given in the data), square the difference, and add them all up.
+
+The program needs to work for any number of cabins and cabin features.
+"""
+import numpy as np
+
+X = np.array([[66, 5, 15, 2, 500], 
+              [21, 3, 50, 1, 100], 
+              [120, 15, 5, 2, 1200]])
+y = np.array([250000, 60000, 525000])
+c = np.array([3000, 200 , -50, 5000, 100])    # coefficient values
+ 
+def squared_error(X, y, c):
+    sse = 0.0
+    for xi, yi in zip(X, y):
+        # add your code here: calculate the predicted price,
+        # subtract it from the actual price yi, 
+        # square the difference using (yi - prediction)**2, 
+        # and add up all the differences in variable sse
+        pass
+
+    print(sse)
+
+squared_error(X, y, c)
+
