4fc3ab6acea2a527b459ee1330d6a14dd3e2fa5b
Muhibul <muhibulalam911@gmail.com>
2026-08-31 23:25:59 +0600

    Add function to find best coefficient set for error
    Implement a function to find the best coefficient set based on squared error.

--- 1.2-Building-AI/chapter_3/exercise_12.py
+++ 1.2-Building-AI/chapter_3/exercise_12.py
@@ -43,3 +43,34 @@ def squared_error(X, y, c):
 
 squared_error(X, y, c)
 
+"""
+Level -  Advance
+
+Write a program that calculates the squared error for multiple sets of coefficient values and prints out the index of the set that yields the smallest squared error: 
+this is a poor man's version of the least squares method where we only consider a fixed set of alternative coefficient vectors instead of finding the global optimum.
+"""
+
+import numpy as np
+
+# data
+X = np.array([[66, 5, 15, 2, 500], 
+              [21, 3, 50, 1, 100], 
+              [120, 15, 5, 2, 1200]])
+y = np.array([250000, 60000, 525000])
+
+# alternative sets of coefficient values
+c = np.array([[3000, 200 , -50, 5000, 100], 
+              [2000, -250, -100, 150, 250], 
+              [3000, -100, -150, 0, 150]])   
+
+def find_best(X, y, c):
+    smallest_error = np.Inf
+    best_index = -1
+    for coeff in c:
+        pass     # edit here: calculate the sum of squared error with coefficient set coeff and
+                 # keep track of the one yielding the smallest squared error
+    print("the best set is set %d" % best_index)
+
+
+find_best(X, y, c)
+
